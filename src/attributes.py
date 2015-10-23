af6aa0922de9bf0913f57a221d903e91c7bee673
David Fialho <fialho.david@gmail.com>
2015-10-23 19:22:38 +0100

    added class for attributes based on AS type and hop count

--- src/attributes.py
+++ src/attributes.py
@@ -39,3 +39,49 @@ class ASTypeAttr(object):
         return self._relationship > other._relationship
 
 
+class HopCountAttr(object):
+
+    def __init__(self, relationship=Relationship.NON, hop_count=0):
+        self._relationship = relationship
+        self._hop_count = hop_count
+
+    @property
+    def value(self):
+        return self._relationship, self._hop_count
+
+    @value.setter
+    def value(self, relationship, hop_count):
+        self._relationship = relationship
+        self._hop_count = hop_count
+
+    def __eq__(self, other):
+        """
+        :type other: HopCountAttr
+        """
+        return self._relationship == other._relationship and self._hop_count == other._hop_count
+
+    def __ne__(self, other):
+        """
+        :type other: HopCountAttr
+        """
+        return self._relationship != other._relationship and self._hop_count != other._hop_count
+
+    def __lt__(self, other):
+        """
+        :type other: HopCountAttr
+        """
+        comparison = self._relationship - other._relationship
+        if comparison == 0:
+            comparison = self._hop_count - other._hop_count
+
+        return comparison < 0
+
+    def __gt__(self, other):
+        """
+        :type other: HopCountAttr
+        """
+        comparison = self._relationship - other._relationship
+        if comparison == 0:
+            comparison = self._hop_count - other._hop_count
+
+        return comparison > 0
