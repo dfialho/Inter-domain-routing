a4e96d7aed8d792c665284ded530cc9316c644e8
David Fialho <fialho.david@gmail.com>
2015-10-24 10:07:03 +0100

    added the realtionship operation table and the operation method

--- src/relationship.py
+++ src/relationship.py
@@ -5,3 +5,15 @@ class Relationship:
     R = 2       # peer-peer relationship
     P = 3       # provider-customer relationship
     NON = 4     # no relationship
+
+    operation_table = [
+        [SELF, SELF, SELF, SELF, SELF],
+        [P, P, P, P, NON],
+        [R, R, NON, NON, NON],
+        [C, C, NON, NON, NON],
+        [NON, NON, NON, NON, NON]
+    ]
+
+    @staticmethod
+    def operation(link_type, path_type):
+        return Relationship.operation_table[link_type][path_type]
