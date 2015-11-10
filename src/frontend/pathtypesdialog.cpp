73ef1cc08e91178e850d10bbd635a395036fd7d1
David Fialho <fialho.david@gmail.com>
2015-11-10 13:54:17 +0000

    introduced the path types dialog

--- src/frontend/pathtypesdialog.cpp
+++ src/frontend/pathtypesdialog.cpp
@@ -1,5 +1,8 @@
 #include "pathtypesdialog.h"
+#include "tablewidget.h"
 #include "ui_pathtypesdialog.h"
+
+#include <QVBoxLayout>
 
 PathTypesDialog::PathTypesDialog(const PathTypesTable &pathTypes, QWidget *parent) :
 	QDialog(parent),
@@ -7,6 +10,47 @@ PathTypesDialog::PathTypesDialog(const PathTypesTable &pathTypes, QWidget *paren
 
 	ui->setupUi(this);
 
+	// create the table widget with 1 column for the path types
+	TableWidget* tableWidget = new TableWidget(pathTypes.size(), 1, this);
+
+	// setup the horizontal header label
+	QStringList horizontalLabels;
+	horizontalLabels << "Prices";
+	tableWidget->setHorizontalHeaderLabels(horizontalLabels);
+
+	// setup the vertical headers with the node's ids
+	QStringList nodeIds;
+	for(auto& nodesPrices : pathTypes) {
+		nodeIds << QString::number(nodesPrices.first);
+	}
+
+	tableWidget->setHorizontalHeaderLabels(horizontalLabels);
+
+	// introduce path types in the table
+	for(unsigned i = 0; i < pathTypes.size(); i++) {
+
+		// convert path type to string
+		QString type;
+		switch(pathTypes[i]) {
+			case PathType::Customer:
+				type = "customer";
+				break;
+			case PathType::Peer:
+				type = "peer";
+				break;
+			case PathType::Provider:
+				type = "provider";
+				break;
+			case PathType::None:
+				type = "none";
+				break;
+		}
+
+		tableWidget->setItem(i, 0, TableWidget::itemFactory(type));
+	}
+
+	this->setLayout(new QVBoxLayout(this));
+	this->layout()->addWidget(tableWidget);
 }
 
 PathTypesDialog::~PathTypesDialog() {
